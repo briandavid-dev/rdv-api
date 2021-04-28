1474e24e4eaae64e89130c82de0654fb386735c2
bob <brayad@gmail.com>
2021-04-28 09:36:40 -0400

    noticia indivisual

--- src/controllers/noticiasController.js
+++ src/controllers/noticiasController.js
@@ -42,6 +42,8 @@ module.exports.getNoticias = (req, res) => {
       };
     });
 
+    results_.shift();
+
     responseAllNoticias = {
       codigo: "1",
       results: results_,
@@ -83,6 +85,63 @@ module.exports.getUltimaNoticia = (req, res) => {
     });
 
     res.json({
+      codigo: "1",
+      results: results_,
+    });
+  });
+};
+
+module.exports.getNoticia = (req, res) => {
+  const { lang, url } = req.params;
+
+  const listLang = ["es", "en"];
+  if (!listLang.includes(lang)) {
+    res.json({
+      codigo: "0",
+      error: "lang fuera de rango",
+    });
+    return;
+  }
+
+  if (url.trim() === "") {
+    res.json({
+      codigo: "0",
+      error: "url fuera de rango",
+    });
+    return;
+  }
+
+  const title = url.replace(/-/g, " ");
+
+  const query = `
+    SELECT id, language, title, summary, name_section, created_at, image_extension, content_html, image_base64
+    FROM posts
+    WHERE language = '${lang}'
+    AND name_page = 'noticias'
+    AND title = '${title}'
+    ORDER BY created_at DESC
+    LIMIT 1;
+    `;
+
+  pool.query(query, function (error, results, fields) {
+    if (error) throw error;
+
+    if (results.length < 1) {
+      res.status(200).json({
+        codigo: "0",
+        mensaje: "Not found",
+      });
+    }
+
+    const results_ = results.map((noticia) => {
+      const url = noticia.title.replace(/ /g, "-");
+      return {
+        url,
+        ...noticia,
+      };
+    });
+
+    res.status(200).json({
       codigo: "1",
       results: results_,
     });
--- src/routes/noticias.js
+++ src/routes/noticias.js
@@ -4,5 +4,6 @@ const noticiasController = require("../controllers/noticiasController");
 
 router.get("/noticias-listado/:lang", noticiasController.getNoticias);
 router.get("/noticias-ultima/:lang", noticiasController.getUltimaNoticia);
+router.get("/noticias/:url/:lang", noticiasController.getNoticia);
 
 module.exports = router;
