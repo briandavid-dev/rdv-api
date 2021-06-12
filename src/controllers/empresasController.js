b63c70a4c12923b31273d2af619a90ca0bf57927
bob <brayad@gmail.com>
2021-06-12 16:42:45 -0400

    ORDER BY title ASC

--- src/controllers/empresasController.js
+++ src/controllers/empresasController.js
@@ -5,7 +5,7 @@ module.exports.get = (req, res) => {
   const { lang } = req.params;
 
   pool.query(
-    `SELECT * FROM posts WHERE language = '${lang}' AND name_page = 'empresas' LIMIT 100`,
+    `SELECT * FROM posts WHERE language = '${lang}' AND name_page = 'empresas' ORDER BY title ASC LIMIT 100`,
     function (error, results, fields) {
       if (error) throw error;
       res.json({ codigo: "1", results });
