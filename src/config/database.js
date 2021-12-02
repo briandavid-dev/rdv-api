482749e3a379dbe97f6a63238025218f956339ab
David Ochoa <david.ochoa@externos-cl.cencosud.com>
2021-12-02 13:04:38 -0300

    conex

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,19 @@
 const mysql = require("mysql");
-// const pool = mysql.createPool({
-//   connectionLimit: 10,
-//   host: "51.161.116.237",
-//   user: "bmosoluc_rdv_user",
-//   password: "RDVrdv..1324",
-//   database: "bmosoluc_rdv_web_cms",
-//   multipleStatements: true,
-// });
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: "127.0.0.1",
-  user: "root",
-  password: "root",
-  database: "rdv",
+  host: "51.161.116.237",
+  user: "bmosoluc_rdv_user",
+  password: "RDVrdv..1324",
+  database: "bmosoluc_rdv_web_cms",
   multipleStatements: true,
 });
+// const pool = mysql.createPool({
+//   connectionLimit: 10,
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: "root",
+//   database: "rdv",
+//   multipleStatements: true,
+// });
 
 module.exports = { pool };
