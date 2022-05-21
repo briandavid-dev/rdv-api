dfe556b5d5f75fa79cea370aec8d2c0c3348ccd0
David Ochoa <david.ochoa@cencosud.cl>
2022-05-20 22:04:14 -0400

    ABOUTUS/feature add management images, - Add all images fron production

--- src/controllers/aboutusController.js
+++ src/controllers/aboutusController.js
@@ -1,14 +1,28 @@
 const fs = require("fs");
+const path = require("path");
 const { pool } = require("../config/database");
+const { v4: uuidv4 } = require("uuid");
+
+function getAllData(res) {
+  pool.query(
+    `SELECT * FROM about_us ORDER BY lang, name_section ASC LIMIT 100`,
+    function (error, results) {
+      if (error) throw error;
+
+      // const data = { en: {}, es: {} };
+
+      // results.forEach((result) => {
+      //   data[result.lang][result.name_section] = result;
+      // });
+
+      res.json({ codigo: "1", results });
+    }
+  );
+}
 
 module.exports.getAll = (req, res) => {
   console.log("BEGIN aboutusController getAll");
-  const { lang } = req.query;
-
-  pool.query(`SELECT * FROM about_us ORDER BY lang, name_section ASC LIMIT 100`, function (error, results, fields) {
-    if (error) throw error;
-    res.json({ codigo: "1", results });
-  });
+  getAllData(res);
 };
 
 module.exports.put = (req, res) => {
@@ -33,20 +47,62 @@ module.exports.put = (req, res) => {
     } = req.body;
 
     const queryUpdate = `
-      UPDATE about_us SET content = '${text1.replace(/'/g, "\\'")}' WHERE name_section = 'text1' AND lang = '${lang}';
-      UPDATE about_us SET content = '${text2.replace(/'/g, "\\'")}' WHERE name_section = 'text2' AND lang = '${lang}';
-      UPDATE about_us SET content = '${text3.replace(/'/g, "\\'")}' WHERE name_section = 'text3' AND lang = '${lang}';
-      UPDATE about_us SET content = '${text4.replace(/'/g, "\\'")}' WHERE name_section = 'text4' AND lang = '${lang}';
-      UPDATE about_us SET content = '${text5.replace(/'/g, "\\'")}' WHERE name_section = 'text5' AND lang = '${lang}';
-      UPDATE about_us SET content = '${text6.replace(/'/g, "\\'")}' WHERE name_section = 'text6' AND lang = '${lang}';
-      UPDATE about_us SET content = '${text7.replace(/'/g, "\\'")}' WHERE name_section = 'text7' AND lang = '${lang}';
-      UPDATE about_us SET content = '${text8.replace(/'/g, "\\'")}' WHERE name_section = 'text8' AND lang = '${lang}';
-      UPDATE about_us SET content = '${text9.replace(/'/g, "\\'")}' WHERE name_section = 'text9' AND lang = '${lang}';
-      UPDATE about_us SET content = '${title1.replace(/'/g, "\\'")}' WHERE name_section = 'title1' AND lang = '${lang}';
-      UPDATE about_us SET content = '${title2.replace(/'/g, "\\'")}' WHERE name_section = 'title2' AND lang = '${lang}';
-      UPDATE about_us SET content = '${title3.replace(/'/g, "\\'")}' WHERE name_section = 'title3' AND lang = '${lang}';
-      UPDATE about_us SET content = '${title4.replace(/'/g, "\\'")}' WHERE name_section = 'title4' AND lang = '${lang}';
-      UPDATE about_us SET content = '${title5.replace(/'/g, "\\'")}' WHERE name_section = 'title5' AND lang = '${lang}';
+      UPDATE about_us SET content = '${text1.replace(
+        /'/g,
+        "\\'"
+      )}' WHERE name_section = 'text1' AND lang = '${lang}';
+      UPDATE about_us SET content = '${text2.replace(
+        /'/g,
+        "\\'"
+      )}' WHERE name_section = 'text2' AND lang = '${lang}';
+      UPDATE about_us SET content = '${text3.replace(
+        /'/g,
+        "\\'"
+      )}' WHERE name_section = 'text3' AND lang = '${lang}';
+      UPDATE about_us SET content = '${text4.replace(
+        /'/g,
+        "\\'"
+      )}' WHERE name_section = 'text4' AND lang = '${lang}';
+      UPDATE about_us SET content = '${text5.replace(
+        /'/g,
+        "\\'"
+      )}' WHERE name_section = 'text5' AND lang = '${lang}';
+      UPDATE about_us SET content = '${text6.replace(
+        /'/g,
+        "\\'"
+      )}' WHERE name_section = 'text6' AND lang = '${lang}';
+      UPDATE about_us SET content = '${text7.replace(
+        /'/g,
+        "\\'"
+      )}' WHERE name_section = 'text7' AND lang = '${lang}';
+      UPDATE about_us SET content = '${text8.replace(
+        /'/g,
+        "\\'"
+      )}' WHERE name_section = 'text8' AND lang = '${lang}';
+      UPDATE about_us SET content = '${text9.replace(
+        /'/g,
+        "\\'"
+      )}' WHERE name_section = 'text9' AND lang = '${lang}';
+      UPDATE about_us SET content = '${title1.replace(
+        /'/g,
+        "\\'"
+      )}' WHERE name_section = 'title1' AND lang = '${lang}';
+      UPDATE about_us SET content = '${title2.replace(
+        /'/g,
+        "\\'"
+      )}' WHERE name_section = 'title2' AND lang = '${lang}';
+      UPDATE about_us SET content = '${title3.replace(
+        /'/g,
+        "\\'"
+      )}' WHERE name_section = 'title3' AND lang = '${lang}';
+      UPDATE about_us SET content = '${title4.replace(
+        /'/g,
+        "\\'"
+      )}' WHERE name_section = 'title4' AND lang = '${lang}';
+      UPDATE about_us SET content = '${title5.replace(
+        /'/g,
+        "\\'"
+      )}' WHERE name_section = 'title5' AND lang = '${lang}';
     `;
 
     pool.query(queryUpdate, [], function (error, results, fields) {
@@ -61,4 +117,59 @@ module.exports.put = (req, res) => {
     });
     res.json({ codigo: "0", message: "error" });
   }
+};
+
+module.exports.updateImageDoc = (req, res) => {
+  console.log("BEGIN updateImageDoc | aboutusController");
+
+  try {
+    const { lang, base64, extension, fieldName, imageAnterior } = req.body;
+
+    if (base64) {
+      const imageName = `${uuidv4()}.${extension}`;
+      const content = Buffer.from(base64, "base64");
+
+      fs.writeFileSync(`./public/aboutus/${imageName}`, content);
+
+      const query = `
+        UPDATE about_us SET content = '${imageName}'
+        WHERE name_section = '${fieldName}' AND lang = '${lang}';
+      `;
+
+      pool.query(query, function (error, results, fields) {
+        if (error) throw error;
+
+        if (imageAnterior !== "") {
+          const path_ = path.join(
+            __dirname,
+            `../../public/aboutus/${imageAnterior}`
+          );
+          if (fs.existsSync(path_)) fs.unlinkSync(path_);
+        }
+
+        getAllData(res);
+      });
+    } else {
+      res.json({ codigo: "0", message: "No se pudo guardar la imagen" });
+    }
+  } catch (error) {
+    fs.writeFile("error.log", error, function (err) {
+      if (err) {
+        return console.log(err);
+      }
+    });
+    res.json({ codigo: "0", message: "error" });
+  }
+};
+
+module.exports.getImage = (req, res) => {
+  console.log("BEGIN getImage | aboutusController");
+  const { fileName } = req.params;
+  const path_ = path.join(__dirname, `../../public/aboutus/${fileName}`);
+
+  if (!fs.existsSync(path_)) {
+    res.status(404).json({ codigo: "0", message: "File not found" });
+  }
+
+  res.sendFile(path_);
 };
--- src/routes/aboutus.js
+++ src/routes/aboutus.js
@@ -4,5 +4,7 @@ const aboutusController = require("../controllers/aboutusController");
 
 router.get("/", aboutusController.getAll);
 router.put("/", aboutusController.put);
+router.put("/image", aboutusController.updateImageDoc);
+router.get("/image/:fileName", aboutusController.getImage);
 
 module.exports = router;
